93d2d57d13ceef3697e4d154630867aa63593db3
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2019-02-28 18:45:49 +0200

    studied try-with-resources

--- src/main/java/org/fasttrackit/persistence/DatabaseConfiguration.java
+++ src/main/java/org/fasttrackit/persistence/DatabaseConfiguration.java
@@ -12,19 +12,18 @@ public class DatabaseConfiguration {
     public static Connection getConnection() throws IOException, ClassNotFoundException, SQLException {
         Properties properties = new Properties();
 
-        InputStream inputStream =
+        // try-with-resources
+        try (InputStream inputStream =
                 DatabaseConfiguration.class
                         .getClassLoader()
-                        .getResourceAsStream("db.properties");
-
-        properties.load(inputStream);
-
-        // load driver class
-        Class.forName(properties.getProperty("DB_DRIVER_CLASS"));
-
-        return DriverManager.getConnection(
-                properties.getProperty("DB_URL"),
-                properties.getProperty("DB_USERNAME"),
-                properties.getProperty("DB_PASSWORD"));
+                        .getResourceAsStream("db.properties")) {
+            properties.load(inputStream);
+            // load driver class
+            Class.forName(properties.getProperty("DB_DRIVER_CLASS"));
+            return DriverManager.getConnection(
+                    properties.getProperty("DB_URL"),
+                    properties.getProperty("DB_USERNAME"),
+                    properties.getProperty("DB_PASSWORD"));
+        }
     }
 }
--- src/main/java/org/fasttrackit/persistence/TopWinnerRepository.java
+++ src/main/java/org/fasttrackit/persistence/TopWinnerRepository.java
@@ -10,16 +10,16 @@ import java.util.List;
 public class TopWinnerRepository {
 
     public void createTopWinner(TopWinner topWinner) throws SQLException, IOException, ClassNotFoundException {
-        Connection connection = DatabaseConfiguration.getConnection();
+        try (Connection connection = DatabaseConfiguration.getConnection()) {
+            String insertSql = "INSERT INTO top_winners (`name`, wonRaces) VALUES (?,?) " +
+                    "ON DUPLICATE KEY UPDATE wonRaces = wonRaces + 1;";
 
-        String insertSql = "INSERT INTO top_winners (`name`, wonRaces) VALUES (?,?) " +
-                "ON DUPLICATE KEY UPDATE wonRaces = wonRaces + 1;";
+            PreparedStatement preparedStatement = connection.prepareStatement(insertSql);
+            preparedStatement.setString(1, topWinner.getName());
+            preparedStatement.setInt(2, topWinner.getWonRaces());
 
-        PreparedStatement preparedStatement = connection.prepareStatement(insertSql);
-        preparedStatement.setString(1, topWinner.getName());
-        preparedStatement.setInt(2, topWinner.getWonRaces());
-
-        preparedStatement.executeUpdate();
+            preparedStatement.executeUpdate();
+        }
     }
 
     public List<TopWinner> getTopWinners() throws SQLException, IOException, ClassNotFoundException {
